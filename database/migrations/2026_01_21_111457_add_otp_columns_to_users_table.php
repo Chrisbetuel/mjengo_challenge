44e88dca1666cb6e3c99a32e37d301a363c07a35
Kalos007 <daviskalokola@gmail.com>
2026-01-22 10:43:39 +0300

    implimentation OTP FUNCT $ migartion updation

--- database/migrations/2026_01_21_111457_add_otp_columns_to_users_table.php
+++ database/migrations/2026_01_21_111457_add_otp_columns_to_users_table.php
@@ -1,31 +1,29 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('otp_code', 6)->nullable();
+            $table->string('otp_code', 6)->nullable()->after('password');
             $table->timestamp('otp_expires_at')->nullable();
-            //
+            $table->unsignedTinyInteger('otp_attempts')->default(0);
+            $table->timestamp('otp_last_sent_at')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['otp_code', 'otp_expires_at']);
-            //
+            $table->dropColumn([
+                'otp_code',
+                'otp_expires_at',
+                'otp_attempts',
+                'otp_last_sent_at',
+            ]);
         });
     }
 };
